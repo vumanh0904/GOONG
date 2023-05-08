af42a85ca7aeb0d21451c312fb63445048019cdc
vumanh0904 <vumanh0904@gmail.com>
2023-05-08 12:54:54 +0700

    them nut dang nhap, goi api, xu ly binding

--- app/src/main/java/com/example/space_smile/screen/SettingScreenActivity.kt
+++ app/src/main/java/com/example/space_smile/screen/SettingScreenActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.databinding.ViewDataBinding
 import com.example.space_smile.R
 import com.example.space_smile.databinding.ActivityMainBinding
+import com.example.space_smile.databinding.ActivitySettingScreenBinding
 import com.example.space_smile.model.Gps
 import com.example.space_smile.service.GPSAPI
 import com.example.space_smile.service.apiService
@@ -13,16 +17,15 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class SettingScreenActivity : AppCompatActivity() {
+//    private  var gpsConnect: Gps = Gps("1",1)
 
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var binding: ActivitySettingScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_setting_screen)
-        val actionBarBackButton = findViewById<ImageView>(R.id.action_bar_back_button)
-            actionBarBackButton.setOnClickListener {
-                onBackPressed()
-            }
+        binding = ActivitySettingScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        binding.tvTitleGPSConnect.text = gpsConnect.number.toString()
 //        lấy dữ liệu từ api
         val getGps = apiService.getInstance().create(GPSAPI::class.java)
 
@@ -30,12 +33,13 @@ class SettingScreenActivity : AppCompatActivity() {
             val result = getGps.getGPS()
             if (result != null){
                 val firstItem = (result.body() as ArrayList<Gps>)[0]
-                val gpsConnect = firstItem.number.toString()
-                binding = ActivityMainBinding.inflate(layoutInflater)
-                setContentView(binding.root)
-//                binding.setting_text_display_gpsConnect_right.setText(gpsConnect)
-
+                 val gpsConnect = firstItem
+                binding.tvTitleGPSConnect.text = gpsConnect.number.toString()
             }
+        }
+        val actionBarBackButton = findViewById<ImageView>(R.id.action_bar_back_button)
+        actionBarBackButton.setOnClickListener {
+            onBackPressed()
         }
     }
 }
